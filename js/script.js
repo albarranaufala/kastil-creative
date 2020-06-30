1c8162b059614a3a172edb657cd4540a1073d44f
Albarra Erdanto <albarraerdanto@gmail.com>
2020-06-30 13:05:33 +0700

    home v1

--- js/script.js
+++ js/script.js
@@ -1,10 +1,13 @@
 const burgerBtn = document.querySelector('.burger');
 const clearBtn = document.querySelector('.clear');
 const navbar = document.querySelector('nav');
+const body = document.querySelector('body');
 
 burgerBtn.addEventListener('click', function(e){
     navbar.classList.add('responsive');
+    body.classList.add('overflow-hidden');
 })
 clearBtn.addEventListener('click', function(e){
     navbar.classList.remove('responsive');
+    body.classList.remove('overflow-hidden');
 })
