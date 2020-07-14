34b289b0ae3e911bdedb7d927efe6b6dd53c3b27
Albarra Erdanto <albarraerdanto@gmail.com>
2020-07-14 19:50:45 +0700

    add to top button

--- js/script.js
+++ js/script.js
@@ -13,49 +13,12 @@ AOS.init({
 burgerBtn.addEventListener('click', function(e) {
     navbar.classList.add('responsive');
     body.classList.add('overflow-hidden');
-})
+});
+
 clearBtn.addEventListener('click', function(e) {
     navbar.classList.remove('responsive');
     body.classList.remove('overflow-hidden');
-})
-
-// document.addEventListener('DOMContentLoaded', _ => {
-//     /*
-//       Quick whip-up of an idea posed by a client: a bar filled with logo's that move to the left slowly and infinitely.
-//       I checked if the <marquee> tag was still working (and it is), but it's considered invalid html now so I needed something else.
-//     */
-//     const containerElem = document.querySelector('.values-container');
-//     const leftSideOfContainer = containerElem.getBoundingClientRect().left;
-//     const listElem = document.querySelector('.values');
-//     let currentLeftValue = 0;
-
-//     // Kick off for the animation function.
-//     window.setInterval(animationLoop, 10);
-
-//     /* 
-//       Looks at first item in the list and checks if it goes out of the visible area 
-//       by comparing the right position of the first list item to the left position 
-//       of the containing element. 
-//     */
-//     function animationLoop() {
-//         const firstListItem = listElem.querySelector('.card-value:first-child');
-
-//         let rightSideOfFirstItem = firstListItem.getBoundingClientRect().right;
-
-//         /* 
-//           If first list item is out of viewable area, move it to the end of the list. 
-//           Also, set the current left value to -1 so we won't stutter.
-//         */
-//         if (rightSideOfFirstItem == leftSideOfContainer) {
-//             currentLeftValue = -1;
-//             listElem.appendChild(firstListItem);
-//         }
-
-//         // The part that keeps it all going: animating the margin left value of the list.
-//         listElem.style.marginLeft = `${currentLeftValue}px`;
-//         currentLeftValue = currentLeftValue - decrementMarquee;
-//     }
-// });
+});
 
 $('.values-container').marquee({
     duration: 28000,
@@ -64,3 +27,30 @@ $('.values-container').marquee({
     duplicated: true,
     startVisible: true
 });
+
+const teamImgs = document.querySelectorAll('.team-img-container');
+teamImgs.forEach(teamImg => {
+    teamImg.addEventListener("mouseover", function(e) {
+        e.target.parentElement.firstElementChild.classList.add('show');
+    })
+    teamImg.addEventListener("mouseout", function(e) {
+        e.target.parentElement.firstElementChild.classList.remove('show');
+    })
+});
+
+toTop = document.querySelector('.to-top');
+
+window.onscroll = function() { scrollFunction() };
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        toTop.style.display = "block";
+    } else {
+        toTop.style.display = "none";
+    }
+}
+
+toTop.addEventListener('click', function(e) {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+})
